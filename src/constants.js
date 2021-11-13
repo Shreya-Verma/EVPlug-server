ffbaa7ffb3e17a8ea9fb3d384d7a316faf0cab00
Shreya Verma <shreya190991@gmail.com>
2021-11-12 22:40:29 -0600

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -5,5 +5,5 @@
 module.exports = Object.freeze({
     PORT:3000,
     JWT_SECRETKEY: 'EV_PLUG_SECRET_KEY',
-    MONGO_URI: 'mongodb+srv://evplug:Qwerty123@cluster0.myes7.mongodb.net/loginDatabase?retryWrites=true&w=majority'
+    MONGO_URI: 'API'
 });
